32d4c2756794f1272710ea80f53b2a4324e885ba
neoninneonin <109036569+neoninneonin@users.noreply.github.com>
2023-07-18 18:33:08 +0300

    Добавляет разметку, стилизацию, сетки

--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 # Личный проект «Мишка» 
 
-* Студент: [Екатерина Юрчак](https://up.htmlacademy.ru/adaptive/28/user/2166929).
-* Наставник: `Неизвестно`.
+* Студент: [Екатерина Юрчак](https://htmlacademy.ru/profile/neonin).
+* Наставник: [Оксана Вайнруб](https://htmlacademy.ru/profile/id166797).
 
 ---
 
---
+++ source/js/script.js
@@ -0,0 +1,67 @@
+const initMenu = () => {
+  const headerToggle = document.querySelector('.header__toggle')
+  const header = document.querySelector('.header')
+
+  header.classList.remove('no-js');
+  headerToggle.addEventListener('click', () => header.classList.toggle('is-open'));
+};
+
+const initModal = () => {
+  var modalButton = document.querySelectorAll(".modal-button");
+
+  if (modalButton.length > 0) {
+    var modal = document.querySelector('.modal');
+
+    for (var i = 0; i < modalButton.length; i++) {
+      modalButton[i].addEventListener('click', (evt) => {
+        evt.preventDefault();
+        modal.classList.remove('modal--close');
+      });
+
+      modal.addEventListener('click', (evt) => {
+        if (evt.target.closest('.modal__container--buy')) {
+          return;
+        }
+        evt.preventDefault();
+        modal.classList.toggle('modal--close');
+      });
+
+      document.addEventListener('keydown', (evt) => {
+        if (evt.key === 'Escape') {
+          evt.preventDefault();
+          modal.classList.toggle('modal--close');
+        }
+      });
+    };
+  };
+};
+
+const initButtonSize = () => {
+  var modalSizeButton = document.querySelectorAll(".modal__size-button");
+  var modal = document.querySelector('.modal');
+
+  for (let i = 0; i < modalSizeButton.length; i++)
+    modalSizeButton[i].onclick = function() {
+    modalSizeButton[i].classList.toggle("modal__size-button--current");
+
+
+    modal.addEventListener('click', (evt) => {
+      if (evt.target.closest('.modal__container--buy')) {
+        return;
+      }
+      evt.preventDefault();
+      modalSizeButton[i].classList.remove('modal__size-button--current');
+    });
+
+    document.addEventListener('keydown', (evt) => {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        modalSizeButton[i].classList.remove('modal__size-button--current');
+      }
+    });
+  };
+};
+
+initMenu();
+initModal();
+initButtonSize();
